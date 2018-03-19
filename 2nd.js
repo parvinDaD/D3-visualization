ecf2cd32212c1b82f907f685e79e0608ae95bda5
wenzi3241 <wenzi3241@gmail.com>
2018-03-19 02:52:09 -0700

    adding force layout in 2nd svg

--- 2nd.js
+++ 2nd.js
@@ -6,7 +6,7 @@ function drawDefault() {
     
     var margin2 = {top: 10, right: 60, bottom: 20, left: 150},
     width2 = 1100 - margin2.left - margin2.right,
-    height2 = 200 - margin2.top - margin2.bottom;
+    height2 = 300 - margin2.top - margin2.bottom;
     
     //used to parse time data on "year" only
    var parseTime = d3.timeParse("%Y");
@@ -239,6 +239,7 @@ function clearCell() {
 //Render cellData and draw sub svg
 function drawCell(margin2, color, yearLower, yearUpper, data) {
     console.log("cellData", data);
+    
    var height = d3.select("#cell").attr("height"),
        width = d3.select("#cell").attr("width");
     
@@ -246,24 +247,24 @@ function drawCell(margin2, color, yearLower, yearUpper, data) {
         .attr('transform', 'translate(' + margin2.left + ',' + margin2.top + ')');
 
     var xScale = 
-        d3.scaleLinear().range([0, width-100]);
-    xScale.domain([yearLower-1, yearUpper]).nice();
+        d3.scaleLinear().range([0, width-100])
+        .domain([yearLower-1, yearUpper]).nice();
     
-   
   
     //Set yscale with padding
-    var yScale = 
-        d3.scaleBand().range([height, 0]).padding(1);
-    yScale.domain(data.map(function(d) {
-            return d.continent;
-        }));
+//    var yScale = 
+//        d3.scaleBand().range([height, 0]).padding(1);
+//    yScale.domain(data.map(function(d) {
+//            return d.continent;
+//        }));
 
     //Set axis without ticks
     var xAxis = d3.axisBottom()
     .tickFormat(d3.format("d"))
     .ticks(10)
-    .scale(xScale).tickSize([]),
-        yAxis = d3.axisLeft().scale(yScale).tickSize([]);
+    .scale(xScale).tickSize([]);
+    
+//    yAxis = d3.axisLeft().scale(yScale).tickSize([]);
     
 
     
@@ -276,42 +277,75 @@ function drawCell(margin2, color, yearLower, yearUpper, data) {
             .attr('class', 'x axis')
             .call(xAxis);
     
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-        .selectAll("text")
-        .attr("font-weight","bold");
-
-    //TODO: adding picture and force overlap
-    var circles = svg.selectAll(".dot")
-          .data(data)
-        .enter().append("circle")
-          .attr("class", "dot")
-          .attr("r", 4)
-          .attr("cx", function(d) {
-              return xScale(d.year2);
-          })
-          .attr("cy", function(d) {
-              return yScale(d.continent);
-          })
-          .style("fill", function(d) { return color(d.continent);})
-          .on("mouseover", function(d) {
-            d3.select(this)
-              tooltip.transition()
-                   .duration(200)
-                   .attr('r',10)
-                   .style("opacity", .5);
-              tooltip.html(d.name+"<br/>"+"Year: "+ d.year2+"<br/>"+"Country: "+d.country+"<br/>"+"Award: "+d.award+" - "+d.cat+"<br/>"+"________________"+"<br/>"+d.Rationale) 
-                   .style("left", (d3.event.pageX - 5.5) + "px")
-                   .style("top", (d3.event.pageY + 1) + "px");
-          })
-          .on("mouseout", function(d) {
-              tooltip.transition()
-                   .duration(500)
-                   .style("opacity", 0);
-          });
+//    svg.append("g")
+//        .attr("class", "y axis")
+//        .call(yAxis)
+//        .selectAll("text")
+//        .attr("font-weight","bold");
+//    
+    //Force layout ref: http://bl.ocks.org/bimannie/cf443db3222b747d3155f8797abc0593
+    var nodes = data.map(function(node, index) {
+        return {
+                index: index,
+                name: node.name,
+                year: node.year2,
+                rationale: node.Rationale,
+                country: node.country,
+                continent: node.continent,
+                award: node.award,
+                x: xScale(node.year2),
+                fx: xScale(node.year2),       
+            };                 
+        });
     
+    
+    var simulation = d3.forceSimulation(nodes)
+        .force("x", d3.forceX(function(d) { return xScale(d.year); }).strength(1))
+        .force("y", d3.forceY(150))
+        .force("collide", d3.forceCollide().radius(25))
+        .force("manyBody", d3.forceManyBody().strength(-10))
+        .stop();
+    
+    for (var i = 0; i < 15; ++i) simulation.tick();
+    
+    
+    //TODO: try to add sort by continent
+    var circle = svg.selectAll(".dot")
+        .data(nodes)
+        .enter().append("circle")
+        .attr("class", "dot")
+        .style("fill", function(d) { return color(d.continent); })
+        .attr("cx", function(d) { return d.x} )
+        .attr("cy", function(d) { return d.y} )
+        .attr("r", 20)
+        .style("opacity", 0.6)
+//            .sort(function(a, b) { return b.dy - a.dy; })
 
+        .on("mouseover", function(d) {
+                d3.select(this)
+                  tooltip.transition()
+                       .duration(200)
+                       .attr('r',10)
+                       .style("opacity", .5);
+                  tooltip.html(d.name+"<br/>"+"Year: "+ d.year+"<br/>"+"Country: "+d.country+"<br/>"+"Award: "+d.award+" - "+d.cat+"<br/>"+"________________"+"<br/>"+d.rationale) 
+                       .style("left", (d3.event.pageX - 5.5) + "px")
+                       .style("top", (d3.event.pageY + 1) + "px");
+              })
+        .on("mouseout", function(d) {
+            tooltip.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+              });
+
+    
+    //TODO: Append image to circle not showing image
+    circle.append("image")
+      .attr("xlink:href", "images/Strawberry.jpg")
+      .attr("class", "circle-image")
+      .attr("x", 0)
+      .attr("y", 0)
+      .attr("width", 20)
+      .attr("height", 20);         
 }
 
 
